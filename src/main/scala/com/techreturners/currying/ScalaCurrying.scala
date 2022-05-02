a0b4ed95a6217c45f952ed0f355f4830560bfa4a
James Heggs <james.heggs@techreturners.com>
2022-05-02 20:00:52 +0100

    Beginning to introduce currying concept

---
+++ src/main/scala/com/techreturners/currying/ScalaCurrying.scala
@@ -0,0 +1,16 @@
+package com.techreturners.currying
+
+object ScalaCurrying {
+
+  // Currying is the technique of translating the evaluation of a function that takes multiple
+  // arguments into evaluating a sequence of functions, each with a single argument.
+  // Its very common to curry functions in other languages such as JavaScript.
+  // It is made possible by being able to assign functions to variables.
+  //
+  // Although the following guide is JavaScript it provides a good description
+  // around some sample use cases of currying
+  // https://dev.to/darkmavis1980/a-practical-example-on-how-to-use-currying-in-javascript-1ae9
+
+
+
+}
