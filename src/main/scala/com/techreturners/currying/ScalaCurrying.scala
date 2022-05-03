ec9d22de8c8c281d2f64f8b05211fb18209c744b
James Heggs <james.heggs@techreturners.com>
2022-05-03 22:45:25 +0100

    Scala syntax examples to include currying

--- src/main/scala/com/techreturners/currying/ScalaCurrying.scala
+++ src/main/scala/com/techreturners/currying/ScalaCurrying.scala
@@ -1,31 +1,46 @@
 package com.techreturners.currying
 
-// Currying is the technique of translating the evaluation of a function that takes multiple
-// arguments into evaluating a sequence of functions, each with a single argument.
-// Its very common to curry functions in other languages such as JavaScript.
-// It is made possible by being able to assign functions to variables.
-//
-// This link provides an example of a practical application of currying
-// https://lukajcb.github.io/blog/scala/2016/03/08/a-real-world-currying-example.html
-
-// Here is another give video outlining practical applications of currying. Although
-// JavaScript it provides a good tutorial of the structure. In fact it's been translated to Scala below
-// https://www.youtube.com/watch?v=fvJ9yWqXcZI
+// A bit of an advanced subject - Currying.
+// https://docs.scala-lang.org/tour/multiple-parameter-lists.html
+// https://www.youtube.com/watch?v=txNAZXPSbiE
 object ScalaCurrying {
 
-  val names = List("Susan", "Jeff", "Colin", "Revathi", "Elena", "Shradha")
+  // Lets assume our intended purpose of the following code is to calculate the final
+  // price of a product. In order to calculate the price of that product
+  // it requires knowledge of a fixed service charge, VAT rate and the product price
+  // It would make sense to create a method something like this:
+  // NON CURRIED EXAMPLE
+  def calculatePriceWithoutCurrying(vatRate: Double, serviceCharge: Double, productPrice: Double): Double =
+    productPrice + productPrice * vatRate + serviceCharge
 
-  def filteringWithoutCurrying(names: Seq[String], nameToMatch: String) : Seq[String] = names.filter(name => name != nameToMatch)
-  
+  // CURRIED EXAMPLE
+  def calculatePriceWithCurrying(vatRate: Double)(serviceCharge: Double)(productPrice: Double): Double =
+    productPrice + productPrice * vatRate + serviceCharge
+
+  // Here we use the underscore to indicate we'll pass on the remaining parameters
+  def calculatePriceWithVatApplied : Double => Double => Double = calculatePriceWithCurrying(0.2) _
+  def calculatePriceWithServiceChargeAndVatApplied : Double => Double = calculatePriceWithVatApplied(10)
+
+
   def printInfo(): Unit = {
     println("\n======")
     println("Reviewing currying - You can see these in the ScalaCurrying.scala file")
     println("======")
 
-    // Notice that in both the examples below
-    // we effectively hard-code the name we wish to filter out
-    println(s"Name out the Colin's without currying: ${filteringWithoutCurrying(names, "Colin" )}")
-    println(s"Name out the Elena's without currying: ${filteringWithoutCurrying(names, "Elena" )}")
+    println(s"Here's the price without currying: ${calculatePriceWithoutCurrying(0.2, 10, 10)}")
+    println(s"Here's another price without currying: ${calculatePriceWithoutCurrying(0.2, 10, 20)}")
+
+    // However notice that we keep repeating the service charge and the VAT rate
+    // Although the values are passed in we're still repeating them. Using currying
+    // we could create a single purpose function that could calculate the product price without
+    // having to remember the VAT rate or service charge. Also it means we'd only have one place to
+    // change the rates if and when they change.
+    println(s"Here's the price WITH currying: ${calculatePriceWithServiceChargeAndVatApplied(10)}")
+    println(s"Here's another price WITH currying: ${calculatePriceWithServiceChargeAndVatApplied(20)}")
+    println(s"And we can still use the original version if we need precise access: ${calculatePriceWithCurrying(0.2)(10)(10)}")
+    println(s"And we could even try it with a different service charge: ${calculatePriceWithVatApplied(20)(10)}")
+
+
   }
 
 }
